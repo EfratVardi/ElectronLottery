e26eb5ae55141915e5bcb8035d86b5656e82e47d
EfratVardi <211.efrat@gmail.com>
2023-12-10 13:32:05 +0200

    change disterbution settings

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function createWindow() {
   mainWindow.loadFile('Main.html')
   mainWindow.menuBarVisible = false
   mainWindow.fullScreen = true;
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
   mainWindow.on('closed', () => {
     mainWindow = null
   })
